ad9e8594315d7589c83e197a72275c02b5ba0a4d
Amitesh Singh <amiteshsinghk@gmail.com>
2025-05-11 00:23:17 +0530

    FactoryPatter

---
+++ app/src/main/java/com/amitesh/designpattern/factoryPattern/FactoryPattern.kt
@@ -0,0 +1,56 @@
+package com.amitesh.designpattern.factoryPattern
+
+
+/*
+* Factory takes care of all the object creational logic. In this pattern, a factory class controls
+* which object to instantiate. Factory pattern comes in handy when dealing with many common objects.
+* You can use it where you might not want to specify a concrete class.
+* */
+
+// Create a factory Dialog which return different dialog instance for different error
+
+
+interface ErrorDialog {
+    fun createDialog(): String
+}
+
+class NetworkDialog : ErrorDialog {
+    override fun createDialog(): String {
+        return "This is network Dialog."
+    }
+}
+
+class ServerErrorDialog : ErrorDialog {
+    override fun createDialog(): String {
+        return "This is Server Error Dialog."
+    }
+}
+
+class LowSpeedDialog : ErrorDialog {
+    override fun createDialog(): String {
+        return "This is Server Error Dialog."
+    }
+}
+
+
+class FactoryPattern {
+
+    enum class ErrorDialogType {
+        NETWORK, SERVER, LOW_SPEED
+    }
+
+    fun createDialogFactory(errorDialogType: ErrorDialogType): ErrorDialog {
+        return when (errorDialogType) {
+            ErrorDialogType.NETWORK -> NetworkDialog()
+            ErrorDialogType.SERVER -> ServerErrorDialog()
+            ErrorDialogType.LOW_SPEED -> LowSpeedDialog()
+        }
+    }
+}
+
+fun main(){
+    var v = FactoryPattern()
+    var network = v.createDialogFactory(FactoryPattern.ErrorDialogType.NETWORK)
+    println(network.createDialog())
+
+}
