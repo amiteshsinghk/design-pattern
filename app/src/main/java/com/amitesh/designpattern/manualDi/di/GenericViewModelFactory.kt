9a3b8f5f3a584a86f68a3033e344fe0a2acaa6e4
Amitesh Singh <amiteshsinghk@gmail.com>
2025-05-12 14:56:57 +0530

    Depedency Injection manual Di injection

--- app/src/main/java/com/amitesh/designpattern/manualDi/di/GenericViewModelFactory.kt
+++ app/src/main/java/com/amitesh/designpattern/manualDi/di/GenericViewModelFactory.kt
@@ -11,3 +11,15 @@ class GenericViewModelFactory<T: ViewModel>(
         return creator() as V
     }
 }
+
+/*
+class GenericViewModelFactory<T: ViewModel>(// Ensures that T must be a subclass of ViewModel.
+    private val creator:() -> T // A lambda function (no-arg function) that returns an instance of type T.This allows you to pass any ViewModel constructor logic into this factory.
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <V : ViewModel> create(modelClass: Class<V>): V { // override the create() method of `ViewModelProvider.Factory`. `V : ViewModel` → This is the return type expected by Android's ViewModel system.
+        return creator() as V // invoke the lambda to get the ViewModel, and cast it to V
+    }
+}
+*/
